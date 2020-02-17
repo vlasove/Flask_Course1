31cfff8135d40a3f562dd31696b2157f3d3f07f4
bgoldovsky <setsunsoul@outlook.com>
2020-02-17 09:49:42 +0300

    initial commit

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# run this command in terminal before chmod +x script.sh
+sudo apt-get update
+
+sudo apt-get -y upgrade
+
+#git
+sudo apt install git
+
+#pip
+sudo apt install python3-pip
+
