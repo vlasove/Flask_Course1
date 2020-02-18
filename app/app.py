300dc23a122e93365a4c8b799d56092c3e2f78e9
vlasove <evgeny_vlasov@yahoo.com>
2020-02-18 12:30:20 +0300

    second app

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask , jsonify
+
+app = Flask(__name__)
+# POST --- получить данные
+# GET --- послать клиенту ответ (данные)
+
+storesDB = [
+    {
+        'name':'SpecialistStore',
+        'items':[
+            {
+                'name' : 'Banana',
+                'price': 70.23
+            },
+            {
+               'name' : 'Apple',
+               'price': 52.23 
+            }
+        ]
+    }
+]
+
+#GET /stores 
+@app.route('/stores', methods=["GET"])
+def get_stores():
+    return jsonify({'stores': storesDB})
+#POST /store + data : {name:}
+#GET /store/<string:name>
+@app.route('/store/<string:name>', methods=["GET"])
+def get_store(name):
+    for store in storesDB:
+        if store["name"] == name:
+            return jsonify(store)
+    return jsonify({"message" : "store not found"})
+
+#POST /store/<string:name>/item {name:, price:}
+#GET /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=["GET"])
+def get_item_in_store(name):
+    .... 
+
+
+
+app.run(port=8080, debug=True)
