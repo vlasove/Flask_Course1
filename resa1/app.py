c41417ddce5b49b07abbd2ea20c564025acae50e
vlasove <evgeny_vlasov@yahoo.com>
2020-02-18 16:33:12 +0300

    added put and post

--- resa1/app.py
+++ resa1/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api 
+from flask_restful import Resource, Api, reqparse 
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,14 +17,46 @@ itemsDB = [
 
 class Item(Resource):
     def get(self, name):
-        return {'item':   next(filter(lambda x: x["name"] == name, itemsDB), None)}
+        return {'item':   next(filter(lambda x: x["name"] == name, itemsDB), None)}, 200
 
     def post(self,name):
-        pass 
+        if next(filter(lambda x: x["name"] == name, itemsDB), None)  is not None:
+            return {"message": "This item already exists"}
+        parser = reqparse.RequestParser()
+        parser.add_argument("price",
+            type=float,
+            required=True,
+            help = "This field cannot be blank!"
+        )
+
+        data = parser.parse_args()
+        new_item = {"name" : name , "price" : data["price"]}
+        itemsDB.append(new_item)
+        return new_item
+
     def delete(self, name):
-        pass 
+        global itemsDB
+        itemsDB = list(filter(lambda  x : x["name"] != name, itemsDB)) 
+        return {"message":"Item deleted"}, 202
+
+
     def put(self, name ):
-        pass 
+        parser = reqparse.RequestParser()
+        parser.add_argument("price",
+            type=float,
+            required=True,
+            help = "This field cannot be blank!"
+        )
+
+        data = parser.parse_args()
+
+        item =  next(filter(lambda x: x["name"] == name, itemsDB), None)
+        if item is None:
+            new_item = {"name": name, "price" : data["price"]}
+            itemsDB.append(new_item)
+        else:
+            item.update(data) #item - referal changing price
+        return item 
 
 class ItemList(Resource):
     def get(self):
@@ -34,3 +66,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
 app.run(port = 8080, debug = True)
+
+
+
---
+++ resa1/test.py
@@ -0,0 +1,8 @@
+a = 2
+
+def add(a):
+    a = a + 2
+    return a 
+
+add(a)
+print(a)
