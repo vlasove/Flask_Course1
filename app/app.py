7abcaf5df68836e2e25cec6c946f996b7571b82e
vlasove <evgeny_vlasov@yahoo.com>
2020-02-18 14:36:51 +0300

    added final with app.route

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify, request
 
 app = Flask(__name__)
 # POST --- получить данные
@@ -23,25 +23,57 @@ storesDB = [
 #GET /stores 
 @app.route('/stores', methods=["GET"])
 def get_stores():
-    return jsonify({'stores': storesDB})
+    return jsonify({'stores': storesDB}), 200
+
+
 #POST /store + data : {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    data = request.get_json()
+    new_store = {
+        'name' : data['name'],
+        'items' : []
+    }
+    storesDB.append(new_store)
+    return jsonify(new_store), 201
+
+
+
+
+
 #GET /store/<string:name>
 @app.route('/store/<string:name>', methods=["GET"])
 def get_store(name):
     for store in storesDB:
         if store["name"] == name:
-            return jsonify(store)
-    return jsonify({"message" : "store not found"})
+            return jsonify(store),200
+    return jsonify({"message" : "store not found"}), 204
 
 #POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>', methods=['POST'])
+def create_item_in_store(name):
+    data = request.get_json()
+    for store in storesDB:
+        if store["name"] == name:
+            new_item = {
+                "name" : data["name"],
+                "price" : data["price"]
+            }
+            store["item"].append(new_item)
+            return jsonify(new_item), 201
+    return jsonify({"message" : "store not found"}), 204
+
+
 #GET /store/<string:name>/item
 @app.route('/store/<string:name>/item', methods=["GET"])
 def get_item_in_store(name):
     for store in storesDB:
         if store["name"] == name:
-            return jsonify({"items":store["items"]})
-    return jsonify({"message":"store not found"})
+            return jsonify({"items":store["items"]}),200
+    return jsonify({"message":"store not found"}), 204
 
 
 
 app.run(port=8080, debug=True)
+
+
