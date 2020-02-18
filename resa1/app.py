6841aaf1e38a6753ba9e19dc696fc0d334ea87d0
vlasove <evgeny_vlasov@yahoo.com>
2020-02-18 15:35:17 +0300

    postman + resources

--- resa1/app.py
+++ resa1/app.py
@@ -1,1 +1,26 @@
-#app.py
+from flask import Flask, request
+from flask_restful import Resource, Api 
+
+app = Flask(__name__)
+api = Api(app)
+
+itemsDB = []
+
+class Item(Resource):
+    def get():
+        pass 
+    def post():
+        pass 
+    def delete():
+        pass 
+    def put():
+        pass 
+
+class ItemList(Resource):
+    def get():
+        pass 
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port = 8080, debug = True)
